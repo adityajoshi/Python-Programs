af34815ebc3eead221c86be7d896eed4d4a792dd
adityajoshi <aditya.joshi1@outlook.com>
2023-07-18 10:09:54 +0530

    update leetcode 217

--- 217_contains_duplicate.py
+++ 217_contains_duplicate.py
@@ -4,10 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        hashset = set()
+        # hashset = set()
+        #
+        # for num in nums:
+        #     if num in hashset:
+        #         return True
+        #     hashset.add(num)
+        # return False
+        return len(set(nums)) != len(nums)
 
-        for num in nums:
-            if num in hashset:
-                return True
-            hashset.add(num)
-        return False
+
+if __name__ == "__main__":
+    c: Solution = Solution()
+    nums = [2, 2, 10, 15]
+    # target = 9
+    output = c.containsDuplicate(nums)
+    print(output)
